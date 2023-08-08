add51e442f046bf36ea7a8dce1c29114467479e0
hasancanmidi <hasancanmidi@hotmail.com>
2023-08-09 01:49:54 +0300

    I've finished the navbar, but it's not responsive right now. i will fix this

---
+++ netflixClone/javascript.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        scroll = $(window).scrollTop();
+        if(scroll > 100){
+            $(".netflix-navbar").css("background", "#0C0C0C");
+        }
+        else{
+            $(".netflix-navbar").css("background","transparent");
+        }
+    })
+})
